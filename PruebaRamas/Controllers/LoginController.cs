7c26b137d4f509a0bbdc42c8168ff42c692f700a
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-09-26 18:24:33 -0600

    Prueba fusionando feature con developer

---
+++ PruebaRamas/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaRamas.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
