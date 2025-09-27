9e06f6eeb8c27c6031708cceccd04847db7fc3b8
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-09-26 18:28:54 -0600

    Prueba fusion 2

---
+++ PruebaRamas/Controllers/Caracteristica1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaRamas.Controllers
+{
+    public class Caracteristica1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
